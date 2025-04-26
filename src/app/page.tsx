ed106683a9ba1a352e04672f327efa191b624045
dogofsunrising <tengbenkouta@gmail.com>
2025-04-26 15:48:19 +0900

    面白いなう

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[60px_1fr_20px] min-h-screen ">
+      <main className="flex flex-col-[140px_1fr] row-start-1">
+        <div className="relative">
+          <div className="absolute left-0">左上に固定する</div>
+        </div>
+      </main>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
